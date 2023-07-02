98c01d19cdf54d083be2fc77f1413d644ab10ee4
宋俊毅 <1815512806@qq.com>
2023-07-02 23:54:49 +0800

    master changes commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git!!!");
         System.out.println("hello git123!!!");
         System.out.println("hello git!!!master");
+        System.out.println("hello git!!!master");
     }
 }
