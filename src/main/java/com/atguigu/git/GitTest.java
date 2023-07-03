7968c3a99d40c4245c5b1880941db680935dc115
SongJunYi-Only <125422860+SongJunYi-Only@users.noreply.github.com>
2023-07-03 10:28:32 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -17,6 +17,6 @@ public class GitTest {
         System.out.println("hello git!!!master");
         System.out.println("hello git!!!hot-fix");
 
-        System.out.println("New push");
+        System.out.println("New push change");
     }
 }
